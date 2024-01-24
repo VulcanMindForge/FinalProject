210e3dbc961c9efec94311ab1ec326f155a3f5c6
Cole Chiga <colejchiga@gmail.com>
2024-01-24 15:23:00 -0700

    fixed problems to get angular up and running

--- DB/vitalityvaultdb.sql
+++ DB/vitalityvaultdb.sql
@@ -7,22 +7,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema vitalityvaultdb
 -- -----------------------------------------------------
-<<<<<<< HEAD
--- 
--- 
--- 
--- \
-=======
->>>>>>> 460350f808944968f22fcf1a071cc532c5fc71d6
 DROP SCHEMA IF EXISTS `vitalityvaultdb` ;
 
 -- -----------------------------------------------------
 -- Schema vitalityvaultdb
---
--- 
--- 
--- 
--- \
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `vitalityvaultdb` DEFAULT CHARACTER SET utf8 ;
 USE `vitalityvaultdb` ;
