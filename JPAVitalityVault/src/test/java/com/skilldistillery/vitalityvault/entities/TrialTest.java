cf8e81f29481eeeed7555fabcada6287f8976278
Cole Chiga <colejchiga@gmail.com>
2024-01-26 08:44:23 -0700

    updated user model

--- JPAVitalityVault/src/test/java/com/skilldistillery/vitalityvault/entities/TrialTest.java
+++ JPAVitalityVault/src/test/java/com/skilldistillery/vitalityvault/entities/TrialTest.java
@@ -46,5 +46,6 @@ class TrialTest {
 		assertNotNull(trial);
 		assertEquals("TEST", trial.getTitle());
 	}
+	
 
 }
--- ngVitalityVault/src/app/models/user.ts
+++ ngVitalityVault/src/app/models/user.ts
@@ -1,23 +1,39 @@
 export class User {
+  // email: string;
+  username: string;
+  password: string;
+  enabled: boolean;
+  role: string;
+  firstName: string;
+  lastName: string;
+  birthdate: string;
+  sex: string;
+  biography: string;
+  imageUrl: string;
 
-    // email: string;
-    username: string;
-    password: string;
-    enabled: boolean;
-    role: string;
-
-    constructor(
-      // email: string = '',
-      username: string = '',
-      password: string = '',
-      enabled: boolean = false,
-      role: string = ''
-    ) {
-      // this.email = email;
-      this.username = username;
-      this.password = password;
-      this.enabled = enabled;
-      this.role = role;
-    }
+  constructor(
+    // email: string = '',
+    username: string = '',
+    password: string = '',
+    enabled: boolean = false,
+    role: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    birthdate: string = '',
+    sex: string = '',
+    biography: string = '',
+    imageUrl: string = ''
+  ) {
+    // this.email = email;
+    this.username = username;
+    this.password = password;
+    this.enabled = enabled;
+    this.role = role;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthdate = birthdate;
+    this.sex = sex;
+    this.biography = biography;
+    this.imageUrl = imageUrl;
   }
-
+}
