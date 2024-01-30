406c366df037a98c79e0b6030a9075295b9da2ad
VulcanMindForge <jastuart@live.com>
2024-01-29 19:25:12 -0700

    Filled out dashboard/landing page

--- ngVitalityVault/src/app/app.component.ts
+++ ngVitalityVault/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { HomeComponent } from "./components/home/home.component";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent, NavBarComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, NavBarComponent, FooterComponent]
 })
 export class AppComponent implements OnInit {
   title = 'ngVitalityVault';
